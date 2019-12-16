321826539ba82d81103384f2e342f3029044d184
erikkallen <erik@evabits.com>
2019-12-16 16:40:02 +0100

    remove usb pins

--- variants/acquaint_pipescanner/variant.h
+++ variants/acquaint_pipescanner/variant.h
@@ -174,9 +174,9 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 /*
  * USB
  */
-//#define PIN_USB_HOST_ENABLE (33ul)
-//#define PIN_USB_DM          (34ul)
-//#define PIN_USB_DP          (35ul)
+#define PIN_USB_HOST_ENABLE (33ul)
+#define PIN_USB_DM          (34ul)
+#define PIN_USB_DP          (35ul)
 /*
  * I2S Interfaces
  */
@@ -228,8 +228,8 @@ extern Uart Serial1;
 //
 // SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
 //                            pins are NOT connected to anything by default.
-#define SERIAL_PORT_USBVIRTUAL      Serial
-#define SERIAL_PORT_MONITOR         Serial
+//#define SERIAL_PORT_USBVIRTUAL      Serial
+#define SERIAL_PORT_MONITOR         Serial1
 // Serial has no physical pins broken out, so it's not listed as HARDWARE port
 #define SERIAL_PORT_HARDWARE        Serial1
 #define SERIAL_PORT_HARDWARE_OPEN   Serial1
